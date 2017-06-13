7442e1b38c1410d788af27a065ce0ccca05328b7
MARCOSRS\Dell-Pc <marcos@umbu.net.br>
2017-06-13 16:45:59 -0300

    1.0

---
+++ AcessoDados/LivroContexto.cs
@@ -0,0 +1,23 @@
+﻿using DemoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DemoCRUD.AcessoDados
+{
+    public class LivroContexto : DbContext
+    {
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Livro> Livros { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Esse comando para de plurarizar as tabelas, como ocorria no Azure
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Properties<string>().Configure(a => a.HasMaxLength(100));
+        }
+    }
+}
